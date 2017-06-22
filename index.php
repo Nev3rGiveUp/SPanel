04f52159e8ac8169388ecd746cd259766356028d
Nev3rGiveUp <samodiv2@gmail.com>
2017-06-23 00:23:58 +0300

    v0.01 basic
    Added:
    Menu
    Simple Slider

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SPanel - Училищен софтуер за следене на учебният процес.</title>
+    <script src="js/jquery.js"></script>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <nav class="main">
+      <ul>
+        <li><a href="#">Начало</a></li>
+        <li><a href="#">Планове</a></li>
+        <li><a href="#">Документация</a></li>
+        <li><a href="#">Контакти</a></li>
+      </ul>
+    </nav>
+
+    <div class="slides">
+      <div class="slide">
+        <img class="bg 1" src="img/sample.png" alt="">
+        <img class="bg 2" src="http://via.placeholder.com/1920x1080?text=Slide1" alt="">
+        <div class="caption">
+          <span class="title">Безплатен план.</span>
+          <span class="body">Не искаме да плащате за нещо, което няма да пасне на нуждите ви.</span>
+        </div>
+      </div>
+    </div>
+    <script src="js/slider.js"></script>
+  </body>
+</html>
---
+++ js/slider.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  $(".slide img").each(function() {
+    if($(this).hasClass("1")){
+      $(this).addClass("active");
+    }
+  });
+  $(".caption").animate({
+    bottom: "0px"
+  }, 500);
+});
+
+
+var slides = 1;
+var a = 0;
+setInterval(function(){
+      if(a == 0){
+        a++;
+      }else{
+        if(a <= slides){
+          a++;
+          nextSlide("Иновативен дизайн.", "Уникално лесният и практичен дизайн на SPanel ви помагат да работите по-лесно", a);
+        }else{
+          a = 1;
+          nextSlide("Иновативен дизайн.", "Уникално лесният и практичен дизайн на SPanel ви помагат да работите по-лесно", a);
+        }
+      }
+    }, 5000);
+
+function captionIn() {
+  $(".caption").animate({
+    bottom: "0px"
+  }, 200);
+}
+
+function captionOut() {
+  $(".caption").animate({
+    bottom: "-250px"
+  }, 200);
+}
+
+function nextSlide(title, body, imageclass) {
+  $(".caption").animate({
+    bottom: "-250px"
+  }, 200, function() {
+    $(".slide img").each(function() {
+      $(this).removeClass("active");
+      $(this).css("opacity", "0");
+    });
+    $(".caption .title").html(title);
+    $(".caption .body").html(body);
+  $(".slide img." + imageclass).animate({
+    opacity: 1
+  }, 500, function () {
+    $(".caption").animate({
+      bottom: "0px"
+    }, 200);
+  });
+    $(".slide img." + imageclass).addClass("active");
+  });
+}
